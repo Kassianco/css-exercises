d5bf8ab1ee217c01bf5c8b18700b86f67054bd25
Cordiner Matteo <cordiner@BallmerMac-01.local>
2019-05-13 17:30:08 +0200

    updates from class

--- Java-Script/Hello-World/script.js
+++ Java-Script/Hello-World/script.js
@@ -24,3 +24,46 @@ var myMessage = messageToTheWorld + space + myName;
 
 var titleElement = document.getElementById("title");
 titleElement.innerHTML = myMessage;
+
+// innerHTML manipulates the content between the HTML
+titleElement.innerHTML = myMessage;
+
+/****************
+ * Manipulate CSS
+****************/
+// select the element with the ID 'styleButton'
+var buttonElement = document.getElementById('styleButton');
+
+// add event listener .addEventListener(type, function)
+buttonElement.addEventListener('click', changeTextColor);
+
+//var counter storing number
+var counter = 0;
+counter = counter+1;
+
+// functiion containing some code):
+function changeTextColor(){
+
+  var titleColors = ["red", "purple", "pink", "orange", "green"];
+  // print the whole Array
+  console.log(titleColors);
+  // Print the Array lenght
+  console.log(titleColors.length);
+  // print the first item in the Array
+  console.log(titleColors[0]);
+  //print the last item in the Array
+  console.log(titleColors[titleColors.length - 1]);
+
+  // assign a new value to the color property
+  titleElement.style.color = titleColors [counter];
+
+  if(counter == titleColors.length - 1) {
+    console.log("whoooo IT IS WORKING");
+    counter = -1;
+  }
+
+  //increment by one
+  counter ++;
+
+  console.log('the function worked');
+};
