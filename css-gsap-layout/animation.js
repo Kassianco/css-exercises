0deab5e4840e569d57866e4e2c6afd456917b8ce
matteocordiner <46818249+Kassianco@users.noreply.github.com>
2019-06-19 21:29:11 +0200

    updates

--- css-gsap-layout/animation.js
+++ css-gsap-layout/animation.js
@@ -7,8 +7,8 @@ TweenMax.from('#textSection', 1.3,{ x: 500, delay:1.2, opacity:0})
 
 
 TweenMax.from('#palmShape', 2,{ y: -500, opacity:0, ease: Bounce. easeOut})
-// TweenMax.from('#blobShape', 2,{ x: -500, opacity:0, ease: Power4. easeOut})
-// TweenMax.from('#textSection', 1.3,{ x: 500, delay:1.2, opacity:0})
+TweenMax.from('#globeShape', 2,{ y: -500, opacity:0, ease: Bounce. easeOut})
+
 
  
 
