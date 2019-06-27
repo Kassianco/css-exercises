4ff51362fe8f47f066067c140bce4743f1df8494
matteocordiner <46818249+Kassianco@users.noreply.github.com>
2019-06-27 10:05:07 +0200

    upd

--- css-gsap-layout/carousele/animation.js
+++ css-gsap-layout/carousele/animation.js
@@ -1,12 +1,3 @@
-
-// TweenMax.from('#dolphineShape', 2,{ y: -500, opacity:0, ease: Bounce. easeOut})
-// TweenMax.from('#blobShape', 2,{ x: -500, opacity:0, ease: Power4. easeOut})
-// TweenMax.from('#textSection', 1.3,{ x: 500, delay:1.2, opacity:0})
-
-// TweenMax.from('#palmShape', 2,{ y: -500, opacity:0, ease: Bounce. easeOut})
-// TweenMax.from('#globeShape', 2,{ y: -500, opacity:0, ease: Bounce. easeOut})
-
-
 var btn1 = document.getElementById('dot1')
 var btn2 = document.getElementById('dot2')
 var btn3 = document.getElementById('dot3')
@@ -21,18 +12,33 @@ climate.style.display = "none";
 
 btn1.classList.add('selected-dot');
 
+TweenMax.from('#dolphineShape', 1.5,{ y: -500, opacity:0, ease: Bounce. easeOut});
+TweenMax.from('#blobShape', 1.5,{ x: -500, opacity:0, ease: Power4. easeOut});
+TweenMax.from('#textSection', 1.3,{ x: 200, delay:.7, opacity:0});
+
 function myFunction() {
   btn1.classList.add('selected-dot');
   btn2.classList.remove('selected-dot');
   btn3.classList.remove('selected-dot');   
     
-  dolphine.style.display = "block";
-  palm.style.display = "none";
-  climate.style.display = "none";
+  // dolphine.style.display = "block";
+  // palm.style.display = "block";
+  // climate.style.display = "none";
+  
+  tl = new TimelineMax();
 
-  TweenMax.from('#dolphineShape', 1.5,{ y: -500, opacity:0, ease: Bounce. easeOut})
-  TweenMax.from('#blobShape', 1.5,{ x: -500, opacity:0, ease: Power4. easeOut})
-  TweenMax.from('#textSection', 1.3,{ x: 200, delay:.7, opacity:0})
+  tl.to('#palmShape', 1.5,{ y: -500, opacity:0})
+  .to('#blobShapePalm', 1.5,{ x: -500, delay:-1.5, opacity:0})
+  .to('#textSectionPalm', 1.3,{ x: 200, delay:-1.5, opacity:0})
+  .from('#dolphineShape', 1.5,{ y: -500, opacity:0, ease: Bounce. easeOut})
+  .from('#blobShapeDolphine', 1.5,{ x: -500, delay:-2.5, opacity:0, ease: Power4. easeOut})
+  .from('#textSectionDolphine', 1.3,{ x: 200, delay:-2.5, opacity:0})
+
+  // if(btn1 || btn3) {
+  //   tl.to('#dolphineShape', 1.5,{ y: -500, opacity:0, ease: Bounce. easeOut})
+  //   .to('#blobShape', 1.5,{ x: -500, delay:-1.5, opacity:0, ease: Power4. easeOut})
+  //   .to('#textSection', 1.3,{ x: 200, delay:-1.5, opacity:0})  
+  // }
 
   console.log('display card 1')
 }
@@ -48,13 +54,25 @@ function myFunction2() {
   btn2.classList.add('selected-dot');
   btn3.classList.remove('selected-dot'); 
 
-  dolphine.style.display = "none";
+  // palm.style.display = "none";
   palm.style.display = "block";
-  climate.style.display = "none";
+  // climate.style.display = "none";
 
-  TweenMax.from('#palmShape', 1.5,{ y: -500, opacity:0, ease: Bounce. easeOut})
-  TweenMax.from('#blobShape', 1.5,{ x: -500, opacity:0, ease: Power4. easeOut})
-  TweenMax.from('#textSection', 1.3,{ x: 200, delay:.7, opacity:0})
+  tl = new TimelineMax();
+
+  tl.to('#dolphineShape', 1.5,{ y: -500, opacity:0,})
+  .to('#blobShapeDolphine', 1.5,{ x: -500, delay:-1.5, opacity:0,})
+  .to('#textSectionDolphine', 1.3,{ x: 200, delay:-1.5, opacity:0})  
+  .from('#palmShape', 1.5,{ y: -500, opacity:0, ease: Bounce. easeOut})
+  .from('#blobShapePalm', 1.5,{ x: -500, delay:-2.5, opacity:0, ease: Power4. easeOut})
+  .from('#textSectionPalm', 1.3,{ x: 200, delay:-2.5, opacity:0})
+
+
+  // if(btn1 || btn3) {
+  //   tl.to('#dolphineShape', 1.5,{ y: -500, opacity:0,})
+  //   .to('#blobShape', 1.5,{ x: -500, delay:-1.5, opacity:0,})
+  //   .to('#textSection', 1.3,{ x: 200, delay:-1.5, opacity:0})  
+  // }
 
   console.log('display card 2')
 }
@@ -71,13 +89,19 @@ function myFunction3() {
   btn3.classList.add('selected-dot');
 
   
-  dolphine.style.display = "none";
-  palm.style.display = "none";
-  climate.style.display = "block";
+  // dolphine.style.display = "none";
+  // palm.style.display = "none";
+  // climate.style.display = "block";
 
-  TweenMax.from('#globeShape', 1.5,{ y: -500, opacity:0, ease: Bounce.easeOut})
-  TweenMax.from('#blobShape', 1.5,{ x: -500, opacity:0, ease: Power4.easeOut})
-  TweenMax.from('#textSection', 1.3,{ x: 200, delay:.7, opacity:0})
+  tl = new TimelineMax();
+
+  tl.from('#globeShape', 1.5,{ y: -500, opacity:0, ease: Bounce. easeOut})
+  .from('#blobShape', 1.5,{ x: -500, delay:-1.5, opacity:0, ease: Power4. easeOut})
+  .from('#textSection', 1.3,{ x: 200, delay:-1.5, opacity:0})
+
+  // TweenMax.from('#globeShape', 1.5,{ y: -500, opacity:0, ease: Bounce.easeOut})
+  // TweenMax.from('#blobShape', 1.5,{ x: -500, opacity:0, ease: Power4.easeOut})
+  // TweenMax.from('#textSection', 1.3,{ x: 200, delay:.7, opacity:0})
 
   console.log('display card 3')
 }
