d4b512b331f226de5095b1cd9640c28f83e69bc6
matteo <matteo.cordiner@gmail.com>
2019-06-27 13:10:16 +0200

    updates

---
+++ js-css-properties/script.js
@@ -0,0 +1,26 @@
+
+
+var shape = document.getElementById('circle')
+
+// const btn = document.getElementById('button')
+// greenBtn.addEventListener('click', function(){
+//   switch() {
+//     case()
+//   }
+// })
+
+function bigAndGreen(){
+  shape.classList.add('big-and-green')
+  console.log('big and green')
+}
+
+function blue(){
+  shape.classList.add('blue')
+  console.log('blue')
+}
+
+function backToBlack() {
+shape.classList.add('back-to-black')
+
+}
+
